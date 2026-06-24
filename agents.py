da0e54bd7d6f70a62240bb9e9563fc1716fa3f65
firaflash <flexflash09@gmail.com>
2026-06-24 16:28:55 +0300

    changed from local model to groq

--- agents.py
+++ agents.py
@@ -1,7 +1,22 @@
+import litellm
+
+original_completion = litellm.completion
+
+def clean_messages(*args, **kwargs):
+    if "messages" in kwargs:
+        for msg in kwargs["messages"]:
+            if isinstance(msg, dict):
+                msg.pop("cache_breakpoint", None)
+
+    return original_completion(*args, **kwargs)
+
+litellm.completion = clean_messages
+
 from crewai import Agent , LLM
-qwen =  LLM(
-    model="ollama/qwen3:8b",
-    base_url="http://localhost:11434"
+llm = LLM(
+    model="groq/llama-3.1-8b-instant",
+    temperature=0.3,
+    max_tokens=800
 )
 
 researcher_agent = Agent(
@@ -10,7 +25,7 @@ researcher_agent = Agent(
     backstory="You are a technical researcher who extracts structured facts.",
     verbose=True, #verbose mode will print out the reasoning and thought process of the agent as it works on the task
     iterations=5, #iterations is the number of times the agent will work on the task before returning a final answer
-    llm=qwen
+    llm=llm
 )
 writer_agent= Agent(
     role = "Technical Writer",
@@ -18,6 +33,6 @@ writer_agent= Agent(
     backstory="You simplify complex information for beginners.",
     verbose=True, #verbose mode will print out the reasoning and thought process of the agent as it works on the task
     iterations=5, #iterations is the number of times the agent will work on the task before returning a final answer
-    llm=qwen # qwen is a large language model that is optimized for reasoning and problem solving
+    llm=llm # qwen is a large language model that is optimized for reasoning and problem solving
 
 )
